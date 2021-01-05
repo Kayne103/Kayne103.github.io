a68f980af8ca6a4534a02c846aebab77fb574ed7
Kayne <holykayne@gmail.com>
2021-01-05 21:47:18 +0200

    More text animation

--- myscripts.js
+++ myscripts.js
@@ -10,3 +10,56 @@ anime.timeline()
     duration: 2250,
     delay: (el, i) => 150 * (i+1)
   });
+
+  var ml4 = {};
+  ml4.opacityIn = [0,3];
+  ml4.scaleIn = [0.5];
+  ml4.scaleOut = 1;
+  ml4.durationIn = 900;
+  ml4.durationOut = 900;
+  ml4.delay = 800;
+
+anime.timeline({loop: true})
+  .add({
+    delay: ml4.delay,
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
